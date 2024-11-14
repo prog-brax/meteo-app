56c6080d2bfc198e19eba31f854ff2c3e0d39e53
Jerome Mare <jerome.mare@inetum.com>
2024-11-14 18:26:18 +0100

    temps de la semaine

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,47 +1,55 @@
 <template>
-  <q-page class="flex flex-center">
-    <q-card class="my-card">
-      <img :src="tempsImage">
-
-      <q-card-section>
-        <div class="text-h6">Météo du jour</div>
-        <div class="text-subtitle2">{{ temps }}</div>
-      </q-card-section>
-
-      <q-card-section class="q-pt-none">
-        {{ lorem }}
-      </q-card-section>
-    </q-card>
-  </q-page>
+  <q-input standout v-model="latitude" label="Latitude" />
+  <q-input standout v-model="longitude" label="Longitude" />
+  <q-btn color="amber" glossy label="Afficher" @click="searchMeteo" />
+  <q-card class="my-card" v-for="(Code, index) in Codes" :key="index">
+    <img
+      :src="codesReference[Code].day.image"
+      height="100"
+      style="width: 100px"
+    />
+    <q-card-section>
+      <div class="text-h6">{{ codesReference[Code].day.description }}</div>
+      <div class="text-subtitle2">by me</div>
+    </q-card-section>
+    <q-card-section class="q-pt-none">
+      {{ lorem }}
+    </q-card-section>
+  </q-card>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
-
-import axios from 'axios'
-
-import wmoCodes from 'src/data/wmo-codes-fr.json'
+import { ref, onMounted } from "vue";
+import axios from "axios";
+import codes from "src/data/wmo-codes-fr.json";
 
 defineOptions({
-  name: 'IndexPage',
-})
+  name: "IndexPage",
+});
 
-const temps = ref('')
-const tempsImage =ref('')
-
-const currentMeteo = ref(null)
-const dailyMeteo = ref(null)
+const meteoD = ref("");
+const imageD = ref();
+const Codes = ref([]);
+const codesReference = ref(codes);
+const latitude = ref("40");
+const longitude = ref("2");
 
 onMounted(async () => {
-  const meteoResults = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,rain,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min`)
+  searchMeteo();
+});
 
-  currentMeteo.value = meteo.data.current
-  dailyMeteo.value = meteo.data.daily
-
-
-
-  temps.value = wmoCodes[code].day.description
-  tempsImage.value = wmoCodes[code].day.image
-})
+async function searchMeteo() {
+  const meteo = await axios.get(
+    "https://api.open-meteo.com/v1/forecast?latitude=" +
+      latitude.value +
+      "&longitude=" +
+      longitude.value +
+      "&daily=weather_code,temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max,uv_index_clear_sky_max&current=weather_code"
+  );
+  const etat = meteo.data.current.weather_code;
+  Codes.value = meteo.data.daily.weather_code;
+  imageD.value = codes[etat].day.image;
+  meteoD.value = codes[etat].day.description;
+}
 
 </script>
