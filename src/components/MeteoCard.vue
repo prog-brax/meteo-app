d16a678438ce086dfd5bab607e7bc9b96a4681ec
Mathys <mathys.inard@yahoo.com>
2025-01-16 19:13:30 +0100

    création de la liste et ajout de la date et de la température

--- src/components/MeteoCard.vue
+++ src/components/MeteoCard.vue
@@ -1,20 +1,28 @@
 <template>
-  <q-card class="my-card">
-    <img
-      :src="codesReference[daily.weatherCode].day.image"
-      height="100"
-      style="width: 100px"
-    />
-    <q-card-section>
-      <div class="text-h6">
-        {{ codesReference[daily.weatherCode].day.description }}
-      </div>
-      <div class="text-subtitle2">{{ daily.temperatureMax }}</div>
-    </q-card-section>
-    <q-card-section class="q-pt-none">
-      {{ lorem }}
-    </q-card-section>
-  </q-card>
+  <div class="q-pa-md" style="max-width: 350px">
+    <q-list bordered>
+      <q-item clickable v-ripple>
+        <q-item-section avatar>
+          <img :src="codesReference[daily.weatherCode].day.image" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>
+            <div class="text-h6">
+              {{ daily.time }}
+            </div>
+          </q-item-label>
+          <q-item-label caption>
+            <div class="text-h6">
+              {{ codesReference[daily.weatherCode].day.description }}
+            </div>
+            <div class="text-h6">
+              {{ daily.temperatureMax }}°-{{ daily.temparatureMin }}°
+            </div>
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </div>
 </template>
 
 <script setup>
